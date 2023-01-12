9855d456ed7da89e102c237ea173fa1c549df8d5
Tobias Kalmbach <t.kalmbach35@gmail.com>
2023-01-12 14:35:15 +0100

    feat: Update run script

--- run.sh
+++ run.sh
@@ -1,10 +1,26 @@
-
+#!/bin/bash
+#SBATCH --ntasks=1
+#SBATCH --time=60:00
+#SBATCH --gres=gpu:1
+#SBATCH --mail-user=tobias.kalmbach@uni-ulm.de
 
 ## cora
-python3 train.py --lr=0.001 --weight_decay=5e-3 --data=cora --alpha=10.0 --hidden=256 --batch_size=2000 --order=2 --tau=2
+# python3 train.py --epochs=1000 --lr=0.001 --weight_decay=5e-3 --data=cora --alpha=10.0 --hidden=256 --batch_size=2000 --order=2 --tau=2
 
 ## citeseer
-python3 train.py --lr=0.001 --weight_decay=5e-3 --data=citeseer --alpha=1.0 --hidden=256 --batch_size=2000 --order=2 --tau=0.5
+python3 train.py --epochs=1000 --lr=0.001 --weight_decay=5e-3 --data=citeseer --alpha=1.0 --hidden=256 --batch_size=2000 --order=2 --tau=0.5
 
 ## pubmed
-python3 train.py --lr=0.1 --weight_decay=5e-3 --data=pubmed --alpha=100 --hidden=256 --batch_size=2000 --order=2 --tau=1
+python3 train.py --epochs=1000 --lr=0.1 --weight_decay=5e-3 --data=pubmed --alpha=100 --hidden=256 --batch_size=2000 --order=2 --tau=1
+
+## ogbn-arxiv
+python3 train.py --epochs=1000 --lr=0.1 --weight_decay=5e-3 --data=ogbn-arxiv --alpha=100 --hidden=256 --batch_size=2000 --order=2 --tau=1
+
+## ogbn-products
+python3 train.py --epochs=1000 --lr=0.1 --weight_decay=5e-3 --data=ogbn-products --alpha=100 --hidden=256 --batch_size=2000 --order=2 --tau=1
+
+## Reddit
+python3 train.py --epochs=1000 --lr=0.1 --weight_decay=5e-3 --data=reddit --alpha=100 --hidden=256 --batch_size=2000 --order=2 --tau=1
+
+## FacebookPagePage
+python3 train.py --epochs=1000 --lr=0.1 --weight_decay=5e-3 --data=facebook --alpha=100 --hidden=256 --batch_size=2000 --order=2 --tau=1
