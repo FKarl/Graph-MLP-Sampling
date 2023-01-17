63c1b07e79c8d6c59d8c95f7ad3f10463399a7b8
Tobias Kalmbach <t.kalmbach35@gmail.com>
2023-01-17 21:25:20 +0100

    feat: Edit dataset calculation

--- DatasetCalculations.py
+++ DatasetCalculations.py
@@ -1,11 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# # Imports
-
-# In[ ]:
-
-
 from ogb.nodeproppred import PygNodePropPredDataset
 from torch_geometric.utils import homophily
 from torch_geometric.datasets import Planetoid, Reddit, FacebookPagePage
@@ -13,13 +8,7 @@ import networkx as nx
 from torch_geometric.utils import to_networkx
 
 
-# 
-
-# ## Calculate Cluster coefficient
-
-# In[ ]:
-
-
+# Calculate Cluster coefficient
 def clustering_coef(data):
     # Convert data to a NetworkX graph
     G = to_networkx(data)
@@ -29,125 +18,46 @@ def clustering_coef(data):
     return average_cc
 
 
-# ## Calculate hompholiy measures
-
-# In[ ]:
-
-
+# Calculate hompholiy measures
 def calc_homophily(data):
     print('Edge Homophily:', homophily(data.edge_index, data.y, method='edge'))
     print('Node Homophily:', homophily(data.edge_index, data.y, method='node'))
     print('Edge Insensitive Homophily:', homophily(data.edge_index, data.y, method='edge_insensitive'))
 
 
-# # Dataset calculations:
-# ## ogbn-arxiv:
-
-# In[ ]:
-
-
-# Download and process data at './dataset/ogbn_arxiv/'
-dataset = PygNodePropPredDataset(name="ogbn-arxiv")
-
-data = dataset[0]
-
-print(data)
-print('Undirected:', data.is_undirected())
-calc_homophily(data)
-print('Average Cluster Coefficient:', clustering_coef(data))
+def calculate_stats(data):
+    print(data)
+    print('Undirected:', data.is_undirected())
+    calc_homophily(data)
+    print('Average Cluster Coefficient:', clustering_coef(data))
 
 
-# ## ogbn-products:
+# Dataset calculations:
+if __name__ == "__main__":
+    # ogbn-arxiv:
+    print("ogbn-arxiv")
+    calculate_stats(PygNodePropPredDataset(name="ogbn-arxiv")[0])
 
-# In[ ]:
+    # ogbn-products:
+    # print("ogbn-products")
+    # calculate_stats(PygNodePropPredDataset(name="ogbn-products")[0])
 
+    # Cora:
+    print("Cora")
+    calculate_stats(Planetoid(root='dataset/Planetoid', name='cora')[0])
 
-# Download and process data at './dataset/ogbn_producs/'
-dataset = PygNodePropPredDataset(name="ogbn-products")
+    # Citeseer:
+    print("Citeseer")
+    calculate_stats(Planetoid(root='dataset/Planetoid', name='Citeseer')[0])
 
-data = dataset[0]
+    # PubMed:
+    print("PubMed")
+    calculate_stats(Planetoid(root='dataset/Planetoid', name='PubMed')[0])
 
-print(data)
-print('Undirected:', data.is_undirected())
-calc_homophily(data)
-print('Average Cluster Coefficient:', clustering_coef(data))
+    # Reddit:
+    print("Reddit")
+    calculate_stats(Reddit(root='dataset/Reddit')[0])
 
-
-# ## Citeseer:
-
-# In[ ]:
-
-
-# Download and process data at './dataset/Planetiod/Citeseer/'
-dataset = Planetoid(root='dataset/Planetoid', name='Citeseer')
-
-data = dataset[0]
-
-print(data)
-print('Undirected:', data.is_undirected())
-calc_homophily(data)
-print('Average Cluster Coefficient:', clustering_coef(data))
-
-
-# ## Pubmed:
-
-# In[ ]:
-
-
-# Download and process data at './dataset/Planetiod/Pubmed/'
-dataset = Planetoid(root='dataset/Planetoid', name='PubMed')
-
-data = dataset[0]
-
-print(data)
-print('Undirected:', data.is_undirected())
-calc_homophily(data)
-print('Average Cluster Coefficient:', clustering_coef(data))
-
-
-# ## Reddit:
-
-# In[ ]:
-
-
-# Download and process data at './dataset/Reddit/'
-dataset = Reddit(root='dataset/Reddit')
-
-data = dataset[0]
-
-print(data)
-print('Undirected:', data.is_undirected())
-calc_homophily(data)
-print('Average Cluster Coefficient:', clustering_coef(data))
-
-
-# ## Facebook:
-
-# In[ ]:
-
-
-# Download and process data at './dataset/FacebookPagePage/'
-dataset = FacebookPagePage(root='dataset/FacebookPagePage')
-
-data = dataset[0]
-
-print(data)
-print('Undirected:', data.is_undirected())
-calc_homophily(data)
-print('Average Cluster Coefficient:', clustering_coef(data))
-
-
-# # Cora
-
-# In[ ]:
-
-
-dataset = Planetoid(root='dataset/Planetoid', name='cora')
-
-data = dataset[0]
-
-print(data)
-print('Undirected:', data.is_undirected())
-calc_homophily(data)
-print('Average Cluster Coefficient:', clustering_coef(data))
-
+    # Facebook:
+    print("Reddit")
+    calculate_stats(FacebookPagePage(root='dataset/FacebookPagePage')[0])
