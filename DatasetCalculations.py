c830e183081c860937b96c8be411721ab9870ba6
Tobias Kalmbach <t.kalmbach35@gmail.com>
2023-01-17 21:45:05 +0100

    feat: Edit dataset calculation

--- DatasetCalculations.py
+++ DatasetCalculations.py
@@ -59,5 +59,5 @@ if __name__ == "__main__":
     calculate_stats(Reddit(root='dataset/Reddit')[0])
 
     # Facebook:
-    print("Reddit")
+    print("Facebook")
     calculate_stats(FacebookPagePage(root='dataset/FacebookPagePage')[0])
