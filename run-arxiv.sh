6bbba462f4245bdc694f70a9c9c6dc31187e9662
Tobias Kalmbach <t.kalmbach35@gmail.com>
2023-02-07 10:25:03 +0100

    feat: Update run scripts

--- run-arxiv.sh
+++ run-arxiv.sh
@@ -7,9 +7,14 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=tobias.kalmbach@uni-ulm.de
 
+SAMPLER="random_batch"
+if [ "$1" != "" ]; then
+    SAMPLER="$1"
+fi
+
 for _ in {1..3}
 do
 
-  python3 train.py --data=ogbn-arxiv --epochs=400 --hidden=2048 --dropout=0.15 --lr=0.001 --weight_decay=0 --alpha=30.0 --batch_size=7000 --order=3 --tau=15 --sampler=$samp
+  python3 train.py --data=ogbn-arxiv --epochs=400 --hidden=2048 --dropout=0.15 --lr=0.001 --weight_decay=0 --alpha=30.0 --batch_size=7000 --order=3 --tau=15 --sampler="$SAMPLER"
 
 done
--- run-reddit.sh
+++ run-reddit.sh
@@ -7,4 +7,10 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=tobias.kalmbach@uni-ulm.de
 
-python3 train.py --data=reddit2 --epochs=400 --hidden=2048 --dropout=0.15 --lr=0.001 --weight_decay=0 --alpha=30.0 --batch_size=7000 --order=3 --tau=15 --sampler=$samp
+
+SAMPLER="random_batch"
+if [ "$1" != "" ]; then
+    SAMPLER="$1"
+fi
+
+python3 train.py --data=reddit2 --epochs=400 --hidden=2048 --dropout=0.15 --lr=0.001 --weight_decay=0 --alpha=30.0 --batch_size=7000 --order=3 --tau=15 --sampler="$SAMPLER"
