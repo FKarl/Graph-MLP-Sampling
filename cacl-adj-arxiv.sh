d51e24ad014f50fba058dd3ae74064e3758b2271
Tobias Kalmbach <t.kalmbach35@gmail.com>
2023-02-09 11:24:55 +0100

    feat: Add adjacency calculation script

---
+++ cacl-adj-arxiv.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks=10
+#SBATCH --cpus-per-task=10
+#SBATCH --time=36:00:00
+#SBATCH --mem=512000
+#SBATCH --partition=fat
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=tobias.kalmbach@uni-ulm.de
+
+python3 train.py --data=ogbn-arxiv --epochs=1 --hidden=2048 --dropout=0.15 --lr=0.001 --weight_decay=0 --alpha=30.0 --batch_size=7000 --order=3 --tau=15
