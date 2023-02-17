9f5b53127232824f6e710fd2ce0a815cd5f90ca6
Tobias Kalmbach <t.kalmbach35@gmail.com>
2023-02-17 12:13:27 +0100

    Update run scripts

--- calc-adj-reddit.sh
+++ calc-adj-reddit.sh
@@ -8,4 +8,4 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=tobias.kalmbach@uni-ulm.de
 
-python3 train.py --data=reddit2 --epochs=1 --hidden=2048 --dropout=0.15 --lr=0.001 --weight_decay=0 --alpha=30.0 --batch_size=7000 --order=3 --tau=15
+python3 train.py --data=reddit2 --epochs=1 --hidden=2048 --dropout=0.15 --lr=0.001 --weight_decay=0 --alpha=30.0 --batch_size=7000 --order=2 --tau=15
